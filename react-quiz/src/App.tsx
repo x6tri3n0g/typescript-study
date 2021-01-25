162d3825d5fd2e35e1115e8f368d6a2eb960e0e2
xtring <hh940630@gmail.com>
2021-01-25 23:53:02 +0900

    Modify: App.tsx

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,8 +1,30 @@
+// Components
+import QuestionCard from './components/QuestionCard'
 
-function App() {
+const App = () => {
+
+  const startTrivia = async() => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div>
-      Hello
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
