56bf98e43d6811b38f0c894694d903e095486910
xtring <hh940630@gmail.com>
2021-03-01 01:07:29 +0900

    Add: Linking toggleTodo

--- ts-react-todo/src/components/TodoListItem/index.tsx
+++ ts-react-todo/src/components/TodoListItem/index.tsx
@@ -6,11 +6,11 @@ type TodoListItemProps = {
     toggleTodo: ToggleTodo;
 };
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleTodo }) => {
     return (
         <li>
             <label className={todo.complete ? 'complete' : undefined}>
-                <input type="checkbox" checked={todo.complete} />
+                <input type="checkbox" checked={todo.complete} onChange={() => toggleTodo(todo)} />
                 {todo.text}
             </label>
         </li>
