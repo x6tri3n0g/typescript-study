0c5821202d7b1e46b30ccbc3c54b1124eb0a3789
xtring <hh940630@gmail.com>
2021-03-01 00:19:28 +0900

    Add: type Todo

--- ts-react-todo/src/App.tsx
+++ ts-react-todo/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { TodoListItem } from './components/TodoListItem';
 
-function App() {
-    return <div>Hello ts</div>;
-}
+type Todo = {
+    text: string;
+    complete: boolean;
+};
+
+const todos: Array<Todo> = [
+    { text: 'Walk the dog', complete: true },
+    { text: 'write App', complete: false },
+];
+
+const App: React.FC = () => {
+    return <TodoListItem />;
+};
 
 export default App;
