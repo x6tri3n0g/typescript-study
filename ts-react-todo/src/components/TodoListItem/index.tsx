78818ac80f04b8c260ba33158d00dc25b4bff979
xtring <hh940630@gmail.com>
2021-03-01 00:18:53 +0900

    Add: TodoListItem

---
+++ ts-react-todo/src/components/TodoListItem/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface TodoListItemProps {
+    todo: {
+        text: string;
+        complete: boolean;
+    };
+}
+
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+    return (
+        <li>
+            <label>
+                <input type="checkbox" checked={todo.complete} />
+                {todo.text}
+            </label>
+        </li>
+    );
+};
