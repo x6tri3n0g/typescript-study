92dcd05932ea0802121aa583e095c81813c8dde9
xtring <hh940630@gmail.com>
2022-08-03 03:23:08 +0900

    update: README

--- ts-handbook/시작하기/greeter.js
+++ ts-handbook/시작하기/greeter.js
@@ -1,45 +1,46 @@
 /*
     코드 컴파일하기
 */
-// function greeter(person: string) {
-//     return "Hello, " + person;
-// }
-// let user = "Jane User";
-// document.body.textContent = greeter(user);
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+document.body.textContent = greeter(user);
 /*
     타입 표기
 */
-// function greeter(person: string) {
-//     return "Hello, " + person;
-// }
-// let user = [0, 1, 2];
-// document.body.textContent = greeter(user);
+function greeter2(person) {
+    return "Hello, " + person;
+}
+var user2 = [0, 1, 2];
+document.body.textContent = greeter2(user2);
 /*
     인터페이스
 */
+function greeter3(person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
+}
+var user3 = { firstName: 'Jane', lastName: 'User' };
+document.body.textContent = greeter3(user3);
+/*
+    클래스
+*/
+// class Student {
+//     fullName: string;
+//     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//         this.fullName = firstName + " " + middleInitial + " " + lastName;
+//     }
+// }
+//
 // interface Person {
 //     firstName: string;
 //     lastName: string;
 // }
+//
 // function greeter(person: Person) {
-//     return 'Hello, ' + person.firstName + ' ' + person.lastName;
+//     return "Hello, " + person.firstName + " " + person.lastName;
 // }
-// let user = { firstName: 'Jane', lastName: 'User' };
+//
+// let user = new Student("Jane", "M.", "User");
+//
 // document.body.textContent = greeter(user);
-/*
-    클래스
-*/
-var Student = /** @class */ (function () {
-    function Student(firstName, middleInitial, lastName) {
-        this.firstName = firstName;
-        this.middleInitial = middleInitial;
-        this.lastName = lastName;
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
-    return Student;
-}());
-function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-var user = new Student("Jane", "M.", "User");
-document.body.textContent = greeter(user);
